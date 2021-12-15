5be551484132e2ec76fb141ccbca834fcc75b9be
45NGC <79377468+45NGC@users.noreply.github.com>
2021-12-15 09:31:27 +0100

    Creacion ProductService.ts Este fichero contiene una funcion que realiza una peticion a red. Invocamos este metodo en la funcion Products de Products.tsx.

--- src/screens/products/Products.tsx
+++ src/screens/products/Products.tsx
@@ -1,17 +1,38 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import { Button, Text } from 'react-native-elements';
+
 // Ejercicio 1
 import Counter from '../../components/counter/counter';
+
+// Ejericicio 2
+import ProductsService from '../../services/ProductsService';
+
 import styles from './product-styles';
 
 const Products = () => {
+  // Crear espacio que pueda contener la informacion de los productos
+
+
   const navigation = useNavigation<any>();
 
   const onPressButton = () => {
     navigation.navigate('product-details');
   };
+
+  // Ejercicio 2
+  useEffect(() => {
+
+    const asyncCall = async () => {
+
+      const result = await ProductsService.getProducts();
+      console.log(result.data);
+      // Almacenar la informacion obtenida en el espacio definido anteriormente
+    };
+    asyncCall();
+
+  }, []) // Este array vacio hace que useEffect solo se invoque una vez
 
   return (
     <SafeAreaView style={styles.container}>
@@ -21,5 +42,4 @@ const Products = () => {
     </SafeAreaView>
   );
 };
-
 export default Products;
