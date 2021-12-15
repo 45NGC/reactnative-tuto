fdcc97cf1d3936d2c81df07df0fc3ba6c9ec754d
45NGC <79377468+45NGC@users.noreply.github.com>
2021-12-15 10:26:36 +0100

    1- Instanciamos un array con los datos del json. 2- Utilizamos una barra de carga mientras no cargen los datos del json. 3- Probamos que este funciona imprimiendo el nombre de un producto en la interdaz.

--- src/screens/products/Products.tsx
+++ src/screens/products/Products.tsx
@@ -1,7 +1,8 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native';
 import { Button, Text } from 'react-native-elements';
+import LinearProgress from 'react-native-elements/dist/linearProgress/LinearProgress';
 
 // Ejercicio 1
 import Counter from '../../components/counter/counter';
@@ -13,6 +14,7 @@ import styles from './product-styles';
 
 const Products = () => {
   // Crear espacio que pueda contener la informacion de los productos
+  const [products, setProducts] = useState<any>([]);
 
 
   const navigation = useNavigation<any>();
@@ -29,14 +31,21 @@ const Products = () => {
       const result = await ProductsService.getProducts();
       console.log(result.data);
       // Almacenar la informacion obtenida en el espacio definido anteriormente
+      setProducts(result.data);
     };
     asyncCall();
 
   }, []) // Este array vacio hace que useEffect solo se invoque una vez
 
+  // Mientras los datos no cargen se muestra una linea de carga (LinearProgress)
+  if (products.length === 0) {
+    <LinearProgress />
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Text h4>Soy la página de productos</Text>
+      <Text h4>{products[0].name}</Text>
       <Counter />
       <Button onPress={onPressButton} title="Ir a la página de detalles" />
     </SafeAreaView>
